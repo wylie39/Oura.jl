9b9dbee6aba531a3bfc213dfce83db90d5ea81a2
Wylie Fowler <WylieFowler@gmail.com>
2025-02-05 22:19:52 -0500

    Add deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,14 @@
-push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, "../src/")
 using Documenter, Oura
 
 
-makedocs(sitename="Oura.jl", pages = [
+makedocs(sitename="Oura.jl", pages=[
     "Home" => "index.md",
     "Get Started" => "getStarted.md",
     "Functions" => "functions.md",
 ])
+
+
+deploydocs(
+    repo="github.com/wylie39/Oura.jl.git",
+)
